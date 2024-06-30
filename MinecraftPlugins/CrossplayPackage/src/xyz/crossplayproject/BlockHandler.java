45a67a2b0fc30d60bf2641bf77887b3144eeff47
The_Atmerek <wiktorobrz@gmail.com>
2024-06-30 17:01:00 +0200

    Added error handling

--- MinecraftPlugins/CrossplayPackage/src/xyz/crossplayproject/BlockHandler.java
+++ MinecraftPlugins/CrossplayPackage/src/xyz/crossplayproject/BlockHandler.java
@@ -24,40 +24,55 @@ public class BlockHandler {
 
     public void setupRoutes(Service spark) {
         spark.get("/blocks", (request, response) -> {
-            int chunkX = Integer.parseInt(request.queryParams("chunkX"));
-            int chunkZ = Integer.parseInt(request.queryParams("chunkZ"));
+            try {
+                String chunkXParam = request.queryParams("chunkX");
+                String chunkZParam = request.queryParams("chunkZ");
 
-            Chunk chunk = Bukkit.getWorlds().get(0).getChunkAt(chunkX, chunkZ);
-            JsonArray blockArray = new JsonArray();
+                if (chunkXParam == null || chunkZParam == null) {
+                    response.status(400);
+                    response.type("application/json");
+                    return "Chunk parameter not found";
+                }
 
-            ChunkSnapshot chunkSnapshot = chunk.getChunkSnapshot(false, true, false);
-            for (int x = 0; x < 16; x++) {
-                for (int z = 0; z < 16; z++) {
-                    for (int y = -1; y <= 319; y++) {
-                        Material material = chunkSnapshot.getBlockType(x, y, z);
-                        if (material.isAir()) {
-                            continue;
+                int chunkX = Integer.parseInt(chunkXParam);
+                int chunkZ = Integer.parseInt(chunkZParam);
+
+                Chunk chunk = Bukkit.getWorlds().get(0).getChunkAt(chunkX, chunkZ);
+                JsonArray blockArray = new JsonArray();
+
+                ChunkSnapshot chunkSnapshot = chunk.getChunkSnapshot(false, true, false);
+                for (int x = 0; x < 16; x++) {
+                    for (int z = 0; z < 16; z++) {
+                        for (int y = -1; y <= 319; y++) {
+                            Material material = chunkSnapshot.getBlockType(x, y, z);
+                            if (material.isAir()) {
+                                continue;
+                            }
+                            JsonObject blockInfo = new JsonObject();
+                            blockInfo.addProperty("type", material.toString());
+                            blockInfo.addProperty("x", x + chunk.getX() * 16);
+                            blockInfo.addProperty("y", y);
+                            blockInfo.addProperty("z", z + chunk.getZ() * 16);
+
+                            BlockData blockData = chunkSnapshot.getBlockData(x, y, z);
+                            blockInfo.addProperty("state", blockData.getAsString());
+
+                            if (biomeSensitiveBlocks.contains(material)) {
+                                Biome biome = chunkSnapshot.getBiome(x, y, z);
+                                blockInfo.addProperty("biome", biome.toString());
+                            }
+
+                            blockArray.add(blockInfo);
                         }
-                        JsonObject blockInfo = new JsonObject();
-                        blockInfo.addProperty("type", material.toString());
-                        blockInfo.addProperty("x", x + chunk.getX() * 16);
-                        blockInfo.addProperty("y", y);
-                        blockInfo.addProperty("z", z + chunk.getZ() * 16);
-
-                        BlockData blockData = chunkSnapshot.getBlockData(x, y, z);
-                        blockInfo.addProperty("state", blockData.getAsString());
-
-                        if (biomeSensitiveBlocks.contains(material)) {
-                            Biome biome = chunkSnapshot.getBiome(x, y, z);
-                            blockInfo.addProperty("biome", biome.toString());
-                        }
-
-                        blockArray.add(blockInfo);
                     }
                 }
+                response.type("application/json");
+                return gson.toJson(blockArray);
+            } catch (NumberFormatException e) {
+                response.status(400);
+                response.type("application/json");
+                return "Chunk parameter is not an integer";
             }
-            response.type("application/json");
-            return gson.toJson(blockArray);
         });
     }
 }
